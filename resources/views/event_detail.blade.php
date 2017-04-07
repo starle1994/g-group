f1875bfc64a0f59ca1cefe3f5f9db684fdb9704f
le star <le star>
2017-04-07 20:57:14 +0700

    a

--- resources/views/event_detail.blade.php
+++ resources/views/event_detail.blade.php
@@ -59,10 +59,10 @@
     </div>
 <script type="text/javascript">
  $(document).ready(function () {  
-        height = $('.groupTop .right .content .main .avatar:first-child img').height().top;        
+        height = $('.groupTop .right .content .main .avatar:first-child img').height();        
         $('.groupTop .right .content .main .avatar img').height(height);  
         $( window ).resize(function() {
-            height = $('.groupTop .right .content .main .avatar:first-child img').height().top;        
+            height = $('.groupTop .right .content .main .avatar:first-child img').height();        
             $('.groupTop .right .content .main .avatar img').height(height);  
         });
 });
